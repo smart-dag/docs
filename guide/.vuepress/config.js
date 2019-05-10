bdb2ef5176cbf93d0619665f91bf6431c5a2f20f
cr4fun <cr4fun@outlook.com>
2019-05-10 18:29:50 +0800

    不同之处

--- guide/.vuepress/config.js
+++ guide/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
         sidebar: [
             ['/', '首页'],
             ['/try', '体验秒级到账'],
+            ['different','不同之处'],
             ['/explorer', '区块链浏览器'],
             ['/wallet', '区块链钱包'],
             ['/diy', 'DIY区块链']
